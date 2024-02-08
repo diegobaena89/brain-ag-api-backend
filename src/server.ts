57c95d90e4087ca268c92cafd6baf4f6f96d31dd
Diego Fronteira <diego.fronteira@ab-inbev.com>
2024-02-08 11:09:12 -0300

    feat: create server.ts file to connect server and changes on eslintrc

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
