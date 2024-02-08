ce6e565b4eea093ddd2875b6f12e2050042d7cc9
Diego Fronteira <diego.fronteira@ab-inbev.com>
2024-02-08 15:40:34 -0300

    refactor: remove threshold

--- jest.config.ts
+++ jest.config.ts
@@ -11,12 +11,4 @@ module.exports = {
     },
   },
   testMatch: ["**/__tests__/**/*.spec.(ts|js)"],
-  coverageTrheshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
 };
